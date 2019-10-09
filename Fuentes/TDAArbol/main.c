e8cbce0a9f520dc5f6aa307c12cf6f4eda8a86a3
David López <dalo_vidpez@hotmail.com.ar>
2019-10-09 17:05:32 -0300

    Añadida función al main que permite imprimir por pantalla el estado actual del árbol

--- Fuentes/TDAArbol/main.c
+++ Fuentes/TDAArbol/main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "arbol.h"
+#include "lista.h"
 
-int main()
-{
-    printf("Hello world!\n");
+void estadoActual(tArbol a);
+void preorden(tNodo n, int nivel);
+
+int main(){
+    tArbol arbol;
+    crear_arbol(&arbol);
+
+    estadoActual(arbol);
     return 0;
+}
+
+void estadoActual(tArbol a){
+    preorden(a->raiz, 0); //0 indica el nivel
+}
+
+void preorden(tNodo n, int nivel){
+    tLista hijos_n = n->hijos; //Lista de nodos
+    tPosicion p = l_primera(hijos_n); //Posición que almacena un nodo
+
+    for(int i=0; i<nivel; i++){
+        printf("\t");
+    }
+    printf("L %p\n", n->elemento);
+
+    while(p != l_ultima(hijos_n)){
+        preorden(p->elemento, nivel+1);
+        p = l_siguiente(hijos_n, p);
+    }
+
 }
